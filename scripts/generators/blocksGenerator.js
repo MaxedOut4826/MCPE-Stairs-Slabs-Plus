646a4375d5f0244ea3467ef65bfcf7677c1a19f0
MaxedOut4826 <maximreed@hotmail.com>
2026-09-03 22:15:43 +0100

    feat: add recipe generation to tool

--- scripts/generators/blocksGenerator.js
+++ scripts/generators/blocksGenerator.js
@@ -1,4 +1,4 @@
-import { KEYWORD_EXCLUSIONS } from "../constants/keywordBlockExclusions.js";
+import { isBlockExcluded } from "../constants/keywordBlockExclusions.js";
 import { NAMESPACE } from "../constants/namespace.js";
 import { blockRenderModeExceptions } from "../renderMethods/blocksRenderManager.js";
 import { isSideBlockFace } from "../helpers/blockFaceUtils.js";
@@ -11,7 +11,6 @@ import {
 import {
     findSubstringInList,
     findSubstringInObjectKeys,
-    isAnySubstringInString,
     replaceTranslationKey,
     generateTranslationKey,
 } from "../helpers/stringUtils.js";
@@ -35,9 +34,6 @@ const TERRAIN_TEXTURE_PATH = SOURCE_DIRECTORY + "terrain_texture.json";
 const VANILLA_TEXTS_DIRECTORY = SOURCE_DIRECTORY + "vanillaTexts";
 const MY_TEXTS_DIRECTORY = SOURCE_DIRECTORY + "myTexts";
 
-const TERRAIN_TEXTURE_OUTPUT =
-    OUTPUT_DIRECTORY + "RP/textures/texture_list.json";
-
 export const terrainTextures = parseJsonFileSync(TERRAIN_TEXTURE_PATH);
 const blocks = parseJsonFileSync(BLOCKS_LIST_PATH);
 const vanillaTexts = parseLangDirectorySync(VANILLA_TEXTS_DIRECTORY);
@@ -50,11 +46,7 @@ const blocksJson = {
     format_version: "1.21.40",
 };
 
-main();
-
-function main() {
-    generateAllBlockClasses(blockClassesRegistry);
-}
+generateAllBlockClasses(blockClassesRegistry);
 
 /**
  * @param {string[]} blockClasses
@@ -64,9 +56,9 @@ function generateAllBlockClasses(blockClasses) {
         generateBlockClass(blockClass);
     }
 
-    writeLangFiles(texts);
+    writeLangFiles(OUTPUT_DIRECTORY + "RP/texts/", texts);
     writeJsonFileSync(OUTPUT_DIRECTORY + "RP/blocks.json", blocksJson);
-    writeTextureList(TERRAIN_TEXTURE_OUTPUT);
+    writeTextureList(OUTPUT_DIRECTORY + "RP/textures/texture_list.json");
 }
 
 /**
@@ -74,9 +66,12 @@ function generateAllBlockClasses(blockClasses) {
  */
 function generateBlockClass(blockClass) {
     for (const [id, vanillaData] of Object.entries(blocks)) {
-        if (!vanillaData || isAnySubstringInString(id, KEYWORD_EXCLUSIONS)) {
+        if (!vanillaData || isBlockExcluded(id)) {
+            console.log("SKIPPED " + id);
             continue;
         }
+
+        console.log("ADD " + id);
 
         const blockBehaviourTemplatePath = `${SOURCE_DIRECTORY}/blocks/${blockClass}.block.json`;
         const blockBehaviourTemplate = parseJsonFileSync(
@@ -106,20 +101,62 @@ function generateBlockClass(blockClass) {
             blockClass,
         );
         generateBlocksJson(namespacedId, vanillaData);
+        generateRecipe(
+            {
+                id: id,
+                newBlockId: newBlockId,
+                namespacedId: namespacedId,
+            },
+            blockClass,
+        );
 
         const blocksOutPath = `${OUTPUT_DIRECTORY}/BP/blocks/${blockClass}/${newBlockId}.block.json`;
         writeJsonFileSync(blocksOutPath, blockBehaviour);
 
-        console.log(namespacedId);
+        // console.log(namespacedId);
     }
+}
+
+/**
+ * @param {Object} idReferences
+ * @param {string} idReferences.id
+ * @param {string} idReferences.newBlockId
+ * @param {string} idReferences.namespacedId
+ * @param {string} blockClass
+ */
+
+function generateRecipe(idReferences, blockType) {
+    const {
+        id: recipeItem,
+        newBlockId: resultItem,
+        namespacedId: resultItemId,
+    } = idReferences;
+    let recipeItemId = recipeItem;
+
+    if (!recipeItemId.startsWith("minecraft:")) {
+        recipeItemId = `minecraft:${recipeItem}`;
+    }
+
+    const recipe = parseJsonFileSync(
+        `${SOURCE_DIRECTORY}/recipes/${blockType}.recipe.json`,
+    );
+    const recipeData = recipe["minecraft:recipe_shaped"];
+    const recipeOutPath = `${OUTPUT_DIRECTORY}/BP/recipes/crafting/${blockType}/${resultItem}.recipe.json`;
+
+    recipeData["key"]["X"]["item"] = recipeItemId;
+    recipeData["unlock"][0]["item"] = recipeItemId;
+    recipeData["description"]["identifier"] = resultItemId;
+    recipeData["result"]["item"] = resultItemId;
+
+    writeJsonFileSync(recipeOutPath, recipe);
 }
 
 /**
  * @param {Record<string, string[]>} texts
  */
-function writeLangFiles(texts) {
+function writeLangFiles(directory, texts) {
     for (const [lang, keys] of Object.entries(texts)) {
-        const textOutputPath = `${OUTPUT_DIRECTORY}/RP/texts/${lang}`;
+        const textOutputPath = directory + lang;
 
         writeFileLinesSync(textOutputPath, keys);
     }
@@ -165,11 +202,7 @@ function generateBlocksJson(id, vanillaData) {
  * @param {string} blockClass
  */
 function generateLangKeys(vanillaTexts, idReferences, blockClass) {
-    const {
-        id: id,
-        newBlockId: newBlockId,
-        namespacedId: namespacedId,
-    } = idReferences;
+    const { id, newBlockId, namespacedId } = idReferences;
 
     for (const [lang, keys] of Object.entries(vanillaTexts)) {
         const searchKey = `tile.${id}.name`;
